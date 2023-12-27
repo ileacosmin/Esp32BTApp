a95948f0ba107db369ca4550f5cb0761638f6e26
Ilea Cosmin Ionut <127050828+pierpatrat@users.noreply.github.com>
2023-12-27 19:55:15 +0200

    Initial commit

---
+++ Esp32 Script/BTLedControl/BTLedControl.ino
@@ -0,0 +1,25 @@
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;
+
+const int ledPin=2;
+
+void setup(){
+  SerialBT.begin("Esp32-BT");
+  pinMode(ledPin,OUTPUT);
+}
+
+void loop(){
+  if(SerialBT.available()){
+    char c = SerialBT.read();
+    if(c=='1')
+    {
+      digitalWrite(ledPin,HIGH);
+    }else if(c=='0')
+    {
+      digitalWrite(ledPin,LOW);
+    }
+  }
+
+
+}
